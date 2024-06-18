8cf1936b1df3c23959b572f3ec9ab809f657f0ad
Mark Felea-Motet <mark.felea-motet@gov.bc.ca>
2024-06-18 08:38:49 -0700

    Add more documents to vector database

--- vector_database.py
+++ vector_database.py
@@ -17,8 +17,12 @@ assistant = client.beta.assistants.create(
 # Create a vector store called "Client Conditions"
 vector_store = client.beta.vector_stores.create(name="Condition Documents")
 
-# Ready the files for upload
-file_paths = ["./test_documents/table_of_conditions_baldy_ridge.pdf", "./test_documents/table_of_conditions.pdf"]
+# Ready the all PDF files in /test_documents for upload
+file_paths = []
+for root, dirs, files in os.walk("./test_documents"):
+    for file in files:
+        file_paths.append(os.path.join(root, file))
+
 file_streams = [open(path, "rb") for path in file_paths]
 
 # Use the upload and poll SDK helper to upload the files, add them to the vector store,
@@ -35,8 +39,17 @@ assistant = client.beta.assistants.update(
   tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
 )
 
-# Example query to retrieve specific conditions
-query = "What conditions are related to First Nations? Mention just the names of the conditions, thier condition number and a brief note on how the condition related to First Nations."
+# search_subject = "water use"
+# search_subject = "transmission lines"
+# search_subject = "moose"
+search_subject = "noise pollution"
+# search_subject = "First Nations/Indigenous/Aboriginal Peoples"
+# search_subject = "forest fires" # Hallucinates a lot because there isn't much about forest fires in the documents
+# search_subject = "air quality"
+# search_subject = "emissions"
+
+# query = f"What are ALL of the conditions related to {search_subject}? Mention just the names of the conditions, thier condition number and the copy the exact part that relates to {search_subject}."
+query = f"What are ALL of the conditions related to {search_subject}? Mention just the names of the conditions and thier condition number."
 
 # Create a thread to handle the query
 thread = client.beta.threads.create(

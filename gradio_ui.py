7bad385db47b932550881623d4a8b6d552433633
Mark Felea-Motet <mark.felea-motet@gov.bc.ca>
2024-07-12 12:03:32 -0700

    Reword search bar title and placeholder

--- gradio_ui.py
+++ gradio_ui.py
@@ -2,9 +2,10 @@ import gradio as gr
 from vector_db_utils import search_for_conditions
 with gr.Blocks() as demo:
 
-    with gr.Tab("Magic RAG Search"):
+    with gr.Tab("Magic Conditions Search"):
 
-        search_topic = gr.Textbox(label="Enter your search topic here")
+        # add placeholder text 
+        search_topic = gr.Textbox(label="Enter a search topic", placeholder="Noise pollution, moose population, air quality, etc.")
         submit_button = gr.Button("Submit")
         output_markdown = gr.Markdown("Markdown output")
 
